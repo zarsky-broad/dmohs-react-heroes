82207b3a764fc81f61c546399d5fe23dc8b1cc60
Isaac Zarsky <zarsky@broadinstitute.org>
2017-12-20 19:19:41 -0500

    mock heroes don't need to be an atom

--- src/cljs/main/dmohs_react/heroes/services/hero_service.cljs
+++ src/cljs/main/dmohs_react/heroes/services/hero_service.cljs
@@ -7,19 +7,18 @@
 (def ^:private live? false)
 
 (defonce ^:private mock-heroes
-         (atom
-          [{:id 11 :name "Mr. Nice"}
-           {:id 12 :name "Narco"}
-           {:id 13 :name "Bombasto"}
-           {:id 14 :name "Celeritas"}
-           {:id 15 :name "Magneta"}
-           {:id 16 :name "RubberMan"}
-           {:id 17 :name "Dynama"}
-           {:id 18 :name "Dr IQ"}
-           {:id 19 :name "Magma"}
-           {:id 20 :name "Tornado"}]))
+         [{:id 11 :name "Mr. Nice"}
+          {:id 12 :name "Narco"}
+          {:id 13 :name "Bombasto"}
+          {:id 14 :name "Celeritas"}
+          {:id 15 :name "Magneta"}
+          {:id 16 :name "RubberMan"}
+          {:id 17 :name "Dynama"}
+          {:id 18 :name "Dr IQ"}
+          {:id 19 :name "Magma"}
+          {:id 20 :name "Tornado"}])
 
-(defonce local-heroes (atom (if live? [] @mock-heroes)))
+(defonce local-heroes (atom (if live? [] mock-heroes)))
 
 (defn get-heroes [on-done]
   (if live?
